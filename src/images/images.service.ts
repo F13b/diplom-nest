aaf77749a7515497c42bcfc8d71bd0e97eb689db
Pavel Kononenkov <p.a.kononenkov@gmail.com>
2023-04-24 12:14:13 +0300

    Добавлены функции обновления и удаления картинок из БД

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,12 +1,27 @@
 import {Injectable} from "@nestjs/common";
 import {PrismaService} from "../prisma.service";
-import {Prisma} from "@prisma/client";
+import {Image, Prisma} from "@prisma/client";
 
 @Injectable()
 export class ImagesService {
     constructor(private prisma: PrismaService) {}
 
-    async add(data: Prisma.ImageCreateInput) {
+    async add(data: Prisma.ImageCreateInput): Promise<Image> {
         return this.prisma.image.create({data});
+    }
+
+    async update(params: {
+        where: Prisma.ImageWhereUniqueInput,
+        data: Prisma.ImageUpdateInput
+    }): Promise<Image> {
+        const {where, data} = params;
+        return this.prisma.image.update({
+            where,
+            data
+        });
+    }
+
+    async delete(where: Prisma.ImageWhereUniqueInput): Promise<Image> {
+        return this.prisma.image.delete({where});
     }
 }
