d20950b63a843c8d140b807d2e4755ce487d6d18
Pavel Kononenkov <p.a.kononenkov@gmail.com>
2023-04-24 11:01:40 +0300

    Добавлен модуль и сервис для работы с картинками, а также написана документация для API пользователя.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,6 @@
 import {Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post} from "@nestjs/common";
 import {UserService} from "./users.service";
-import {Prisma, Users as UsersModel} from "@prisma/client";
+import {Users as UsersModel} from "@prisma/client";
 import {ApiOperation, ApiParam, ApiResponse, ApiTags} from "@nestjs/swagger";
 
 @ApiTags('Users')
@@ -8,6 +8,9 @@ import {ApiOperation, ApiParam, ApiResponse, ApiTags} from "@nestjs/swagger";
 export class UsersController {
     constructor(private readonly usersService: UserService) {}
 
+    /**
+     * Функция обращается к функции users класса UsersService, который осуществляет выборку пользователей из БД
+     */
     @Get()
     @ApiOperation({ summary: "Selects all users" })
     @ApiResponse({ status: HttpStatus.OK, description: "Success" })
@@ -16,6 +19,11 @@ export class UsersController {
         return this.usersService.users({});
     }
 
+    /**
+     * Функция обращается к функции user класса UsersService, куда передает id пользователя и
+     * осуществляет выборку из БД по идентификатору пользователя
+     * @param id - уникальный идентификатор пользователя
+     */
     @Get(':userId')
     @ApiOperation({ summary: "Searches for a user by id" })
     @ApiParam({ name: "userId", required: true, description: "User identifier" })
@@ -25,8 +33,16 @@ export class UsersController {
         return this.usersService.user({id: Number(id)});
     }
 
+    /**
+     * Функция обращается к функции createUser класса UsersService, куда передает
+     * объект с данными для создания записи пользователя из БД
+     * @param {UsersModel} userData - объект с типом UsersModel, который содержит в себе данные пользователя
+     */
     @Post('create')
-    async signupUser(
+    @ApiOperation({ summary: "Creates a new user" })
+    @ApiResponse({ status: HttpStatus.OK, description: "Success" })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+    async createUser(
         @Body() userData: {
             email: string,
             name: string,
@@ -58,8 +74,19 @@ export class UsersController {
         });
     }
 
+    /**
+     * Функция обращается к функции updateUser класса UsersService, куда
+     * передает id пользователя и объект с данными для обновления записи пользователя из БД
+     * @param {string} id - уникальный идентификатор пользователя
+     * @param {UsersModel} userData - объект с типом UsersModel, который содержит в себе данные пользователя
+     */
     @Patch(':userId')
-    updateUser(
+    @ApiOperation({ summary: "Updates the user by his id" })
+    @ApiParam({ name: "userId", required: true, description: "User identifier" })
+    @ApiParam({ name: "userData", required: true, description: "New user data" })
+    @ApiResponse({ status: HttpStatus.OK, description: "Success" })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+    async updateUser(
         @Param('userId') id: string,
         @Body() userData: {
             email?: string,
@@ -87,8 +114,17 @@ export class UsersController {
         });
     }
 
+    /**
+     * Функция обращается к функции DeleteUser класса UsersService, куда
+     * передает объект с параметрами для удаления записи пользователя из БД
+     * @param {string} id - уникальный идентификатор пользователя
+     */
     @Delete(':userId')
-    deleteUser(
+    @ApiOperation({ summary: "Deleting a user by id" })
+    @ApiParam({ name: "userId", required: true, description: "User identifier" })
+    @ApiResponse({ status: HttpStatus.OK, description: "Success" })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+    async deleteUser(
         @Param('userId') id: string
     ): Promise<UsersModel> {
         return this.usersService.deleteUser({id: Number(id)})
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,6 +7,12 @@ import * as bcrypt from 'bcrypt';
 export class UserService {
     constructor(private prisma: PrismaService) {}
 
+    /**
+     * Функция обращается к объекту Users ORM Prisma и вызывает функцию findUnique, в которую передает объект с парой ключ:значние,
+     * где: ключ - поле в таблице пользователя, а значение - содержимое поля
+     * @param {Prisma.UsersWhereUniqueInput} userWhere - объект с парой ключ:значение для
+     * поиска необходимой записи в таблице пользователя
+     */
     async user(
         userWhere: Prisma.UsersWhereUniqueInput
     ): Promise<Users | null> {
@@ -15,6 +21,11 @@ export class UserService {
         });
     }
 
+    /**
+     * Функция обращается к объекту Users ORM Prisma и вызывает функцию findMany, в которую передает
+     * объект с настройками выборки записей
+     * @param params - объект с настройками выборки
+     */
     async users(params: {
         skip?: number;
         take?: number;
@@ -22,7 +33,13 @@ export class UserService {
         where?: Prisma.UsersWhereInput;
         orderBy?: Prisma.UsersOrderByWithRelationInput;
     }): Promise<Users[]> {
-        const { skip, take, cursor, where, orderBy } = params;
+        const {
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy
+        } = params;
         return this.prisma.users.findMany({
             skip,
             take,
@@ -32,6 +49,10 @@ export class UserService {
         });
     }
 
+    /**
+     * Функция обращается к объекту Users ORM Prisma и вызывает функцию create, в которую передает объект с данными пользователя
+     * @param {Prisma.UsersCreateInput} data - объект с данными пользователя
+     */
     async createUser(data: Prisma.UsersCreateInput): Promise<Users> {
         const saltRounds: number = 10;
         const hash = await bcrypt.hash(data.password, saltRounds);
@@ -41,6 +62,11 @@ export class UserService {
         });
     }
 
+    /**
+     * Функция обращается к объекту Users ORM Prisma и вызывает функцию update, в которую передает объект с
+     * объектом настроек поиска записи в таблице пользователя и объект с данными пользователя
+     * @param params - объект с настройками поиска записи пользователя в БД и данными для обновления
+     */
     async updateUser(params: {
         where: Prisma.UsersWhereUniqueInput;
         data: Prisma.UsersUpdateInput;
@@ -52,6 +78,11 @@ export class UserService {
         });
     }
 
+    /**
+     * Функция обращается к объекту Users ORM Prisma и вызывает функцию delete, в которую передает объект с парой ключ:значние,
+     * где: ключ - поле в таблице пользователя, а значение - содержимое поля
+     * @param {Prisma.UsersWhereUniqueInput} where - объект с парой ключ:значения для поиска необходимой записи в таблице пользователя
+     */
     async deleteUser(where: Prisma.UsersWhereUniqueInput): Promise<Users> {
         return this.prisma.users.delete({
             where,
